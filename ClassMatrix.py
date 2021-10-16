48039b0217345db4af43531393964d293ef57d17
nikitaqqqq <76213843+nikitaqqqq@users.noreply.github.com>
2021-10-17 01:16:01 +0500

    Update ClassMatrix.py

--- ClassMatrix.py
+++ ClassMatrix.py
@@ -1,3 +1,22 @@
+from sys import stdout
+from sys import stdin
+def get():
+    return stdin.readline().strip()
+def getf(sp = " "):
+    return [int(i) for i in get().split(sp)]
+def put(a, end = "\n"):
+    stdout.write(str(a) + end)
+def putf(a, sep = " ", end = "\n"):
+    stdout.write(sep.join([str(i) for i in a]) + end)
+     
+#from collections import defaultdict as dd, deque
+#from random import randint, shuffle, sample
+#from functools import cmp_to_key, reduce
+#from math import factorial as fac, acos, asin, atan2, gcd, log, e
+#from bisect import bisect_right as br, bisect_left as bl, insort
+
+MOD = 10 ** 9 + 7
+
 class Matrix:
     def __init__(self, n, m):
         self.M = [[0 for j in range(m)] for i in range(n)]
@@ -10,7 +29,7 @@ class Matrix:
         n, m = self.size
         for i in range(n):
             for j in range(m):
-                self.M[i][j] = arr[i][j]
+                self.M[i][j] = arr[i][j] % MOD
  
     def __add__(self, other):
         if(self.size != other.size):
@@ -22,6 +41,7 @@ class Matrix:
             for i in range(n):
                 for j in range(m):
                     self.M[i][j] += other.M[i][j]
+                    self.M[i][j] %= MOD
         return self
  
     def __mul__(self, other):
@@ -37,4 +57,24 @@ class Matrix:
                 for j in range(m):
                     for k in range(k1):
                         c.M[i][j] += self.M[i][k] * other.M[k][j]
+                        c.M[i][j] %= MOD
             return c
+
+    def __pow__(self, n):
+        if(n == 1):
+            return self
+        if(n % 2 == 0):
+            a = pow(self, n // 2)
+            return a * a
+        return self * pow(self, n - 1)
+
+def main():
+    p = Matrix(2, 2)
+    p.fill([[0, 1], [1, 1]])
+    #print(p)
+    #print(pow(p, 2))
+    a = Matrix(1, 2)
+    a.fill([[0, 1]])
+    print(a)
+    print(a * pow(p, 5))
+main()
