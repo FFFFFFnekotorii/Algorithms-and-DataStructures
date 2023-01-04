84857b982f7289da19b26bff61742e7fe1b68837
Nikita <76213843+nikitaqqqq@users.noreply.github.com>
2023-01-04 15:16:23 +0500

    Update good_segment_tree.cpp

--- good_segment_tree.cpp
+++ good_segment_tree.cpp
@@ -34,88 +34,88 @@ typedef long double ld;
 //#define ONLINE_JUDGE
 
 #ifndef ONLINE_JUDGE
-	#define cout cerr
+    #define cout cerr
 #endif
 
 struct segment_tree_node_minmax {
 
-	ll min_val, max_val, min_ind, max_ind;
+    ll min_val, max_val, min_ind, max_ind;
 
     segment_tree_node_minmax() {
         min_val = INF;
-		max_val = -INF;
-		min_ind = -1;
-		max_ind = -1;
+        max_val = -INF;
+        min_ind = -1;
+        max_ind = -1;
     }
-	
-	segment_tree_node_minmax(ll ind0, ll val0) {
+
+    segment_tree_node_minmax(ll ind0, ll val0) {
         min_val = val0;
-		max_val = val0;
-		min_ind = ind0;
-		max_ind = ind0;
+        max_val = val0;
+        min_ind = ind0;
+        max_ind = ind0;
     }
-	
-	segment_tree_node_minmax operator|(segment_tree_node_minmax other) {
+
+    segment_tree_node_minmax operator|(segment_tree_node_minmax other) {
         segment_tree_node_minmax res;
-		
-		if (min_val < other.min_val) {
-			res.min_val = min_val;
-			res.min_ind = min_ind;
-		} else {
-			res.min_val = other.min_val;
-			res.min_ind = other.min_ind;
-		}
-		
-		if (max_val < other.max_val) {
-			res.max_val = other.max_val;
-			res.max_ind = other.max_ind;
-		} else {
-			res.max_val = max_val;
-			res.max_ind = max_ind;
-		}
-		
-		return res;
+
+        if (min_val < other.min_val) {
+            res.min_val = min_val;
+            res.min_ind = min_ind;
+        } else {
+            res.min_val = other.min_val;
+            res.min_ind = other.min_ind;
+        }
+
+        if (max_val < other.max_val) {
+            res.max_val = other.max_val;
+            res.max_ind = other.max_ind;
+        } else {
+            res.max_val = max_val;
+            res.max_ind = max_ind;
+        }
+
+        return res;
     }
-	
-	void operator+=(ll delta) {
-		min_val += delta;
-		max_val += delta;
-	}
-	
-	void operator=(ll new_val) {
-		min_val = new_val;
-		max_val = new_val;
-	}
+
+    void operator+=(ll delta) {
+        min_val += delta;
+        max_val += delta;
+    }
+
+    void operator=(ll new_val) {
+        min_val = new_val;
+        max_val = new_val;
+    }
 
 };
 
 struct segment_tree_node_sum {
 
-	ll val;
+    ll val;
 
     segment_tree_node_sum() {
         val = 0;
     }
-	
-	segment_tree_node_sum(ll ind0, ll val0) {
+
+    segment_tree_node_sum(ll ind0, ll val0) {
         val = val0;
     }
-	
-	segment_tree_node_sum operator|(segment_tree_node_sum other) {
+
+    segment_tree_node_sum operator|(segment_tree_node_sum other) {
         segment_tree_node_sum res;
-		
-		res.val = val + other.val;
-		
-		return res;
+
+        res.val = val + other.val;
+
+        return res;
     }
-	
-	void operator+=(ll delta) {
-		val += delta;
-	}
-	
-	void operator=(ll new_val) {
-		val = new_val;
-	}
+
+    void operator+=(ll delta) {
+        val += delta;
+    }
+
+    void operator=(ll new_val) {
+        val = new_val;
+    }
 
 };
 
@@ -160,41 +160,41 @@ struct segment_tree {
     segment_tree_node get(ll ql, ll qr) {
         return get(0, 0, tree.size() >> 2, ql, qr);
     }
-	
-	void add(ll v, ll l, ll r, ll ind, ll delta) {
+
+    void add(ll v, ll l, ll r, ll ind, ll delta) {
         if (r - l == 1) {
-			tree[v] += delta;
-			return ;
-		}
+            tree[v] += delta;
+            return ;
+        }
         ll m = (l + r) / 2;
-		
-		if (ind < m) {
-			add(2 * v + 1, l, m, ind, delta);
-		} else {
-			add(2 * v + 2, m, r, ind, delta);
-		}
-		
-		tree[v] = tree[2 * v + 1] | tree[2 * v + 2];
+
+        if (ind < m) {
+            add(2 * v + 1, l, m, ind, delta);
+        } else {
+            add(2 * v + 2, m, r, ind, delta);
+        }
+
+        tree[v] = tree[2 * v + 1] | tree[2 * v + 2];
     }
 
     void add(ll ind, ll delta) {
         add(0, 0, tree.size() >> 2, ind, delta);
     }
-	
-	void update(ll v, ll l, ll r, ll ind, ll new_val) {
+
+    void update(ll v, ll l, ll r, ll ind, ll new_val) {
         if (r - l == 1) {
-			tree[v] = new_val;
-			return ;
-		}
+            tree[v] = new_val;
+            return ;
+        }
         ll m = (l + r) / 2;
-		
-		if (ind < m) {
-			update(2 * v + 1, l, m, ind, new_val);
-		} else {
-			update(2 * v + 2, m, r, ind, new_val);
-		}
-		
-		tree[v] = tree[2 * v + 1] | tree[2 * v + 2];
+
+        if (ind < m) {
+            update(2 * v + 1, l, m, ind, new_val);
+        } else {
+            update(2 * v + 2, m, r, ind, new_val);
+        }
+
+        tree[v] = tree[2 * v + 1] | tree[2 * v + 2];
     }
 
     void update(ll ind, ll new_val) {
@@ -205,78 +205,78 @@ struct segment_tree {
 void solve() {
 
     ll n, i, m, ans, x, val;
-	vector<ll> a, p;
-	segment_tree<segment_tree_node_minmax> st;
-	segment_tree<segment_tree_node_sum> st_add;
-	
-	cin >> n >> m;
-	m--;
-	
-	a.resize(n);
-	p.resize(n);
-	st.resize(n);
-	st_add.resize(n);
-	
-	for (i = 0; i < n; i++) {
-		cin >> a[i];
-	}
-	
-	p[0] = a[0];
-	for (i = 1; i < n; i++) {
-		p[i] = p[i - 1] + a[i];
-	}
-	
-	st.build(a);
-	
-	ans = 0;
-	
-	for (i = m - 1; -1 < i; i--) {
-		
-		while (p[i] + st_add.get(0, i + 1).val < p[m] + st_add.get(0, m + 1).val) {
-			x = st.get(i + 1, m + 1).max_ind;
-			st.update(x, -a[x]);
-			
-			st_add.add(x, -2 * a[x]);
-			
-			a[x] = -a[x];
-			
-			ans += 1;
-		}
-	}
-	
-	for (i = m + 1; i < n; i++) {
-		
-		while (p[i] + st_add.get(0, i + 1).val < p[m] + st_add.get(0, m + 1).val) {
-			x = st.get(m + 1, i + 1).min_ind;
-			st.update(x, -a[x]);
-			
-			st_add.add(x, -2 * a[x]);
-			
-			a[x] = -a[x];
-			
-			ans += 1;
-		}
-	}
-	
-	cout << ans << '\n';
+    vector<ll> a, p;
+    segment_tree<segment_tree_node_minmax> st;
+    segment_tree<segment_tree_node_sum> st_add;
+
+    cin >> n >> m;
+    m--;
+
+    a.resize(n);
+    p.resize(n);
+    st.resize(n);
+    st_add.resize(n);
+
+    for (i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    p[0] = a[0];
+    for (i = 1; i < n; i++) {
+        p[i] = p[i - 1] + a[i];
+    }
+
+    st.build(a);
+
+    ans = 0;
+
+    for (i = m - 1; -1 < i; i--) {
+
+        while (p[i] + st_add.get(0, i + 1).val < p[m] + st_add.get(0, m + 1).val) {
+            x = st.get(i + 1, m + 1).max_ind;
+            st.update(x, -a[x]);
+
+            st_add.add(x, -2 * a[x]);
+
+            a[x] = -a[x];
+
+            ans += 1;
+        }
+    }
+
+    for (i = m + 1; i < n; i++) {
+
+        while (p[i] + st_add.get(0, i + 1).val < p[m] + st_add.get(0, m + 1).val) {
+            x = st.get(m + 1, i + 1).min_ind;
+            st.update(x, -a[x]);
+
+            st_add.add(x, -2 * a[x]);
+
+            a[x] = -a[x];
+
+            ans += 1;
+        }
+    }
+
+    cout << ans << '\n';
 }
 
 int main() {
 
-	#ifdef ONLINE_JUDGE
-		INPUT;
-	#endif
+    #ifdef ONLINE_JUDGE
+        INPUT;
+    #endif
 
     int t;
 
     for (cin >> t; t; t--) {
-		solve();
+        solve();
     }
 
-	#ifndef ONLINE_JUDGE
-		cerr << '\n';
-		system("pause");
-	#endif
+    #ifndef ONLINE_JUDGE
+        cerr << '\n';
+        system("pause");
+    #endif
 
     return 0;
 }
