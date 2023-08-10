a5a3e02581e7a586d67bc458a078203ada2e10d5
Nikita <76213843+nikitaqqqq@users.noreply.github.com>
2023-08-11 02:09:41 +0500

    Update treap_with_size.cpp

--- C++/treap_with_size.cpp
+++ C++/treap_with_size.cpp
@@ -1,120 +1,181 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <string>
-#include <cmath>
-#include <set>
-#include <cstring>
-#include <deque>
+#include <sstream>
 #include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <bitset>
 #include <random>
 
-#define iter vector<long long int>::iterator
-#define input() ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#include <chrono>
+#include <cassert>
 
 using namespace std;
 
 typedef long long int ll;
+typedef long double ld;
 
-struct Node{
-    ll key, prior;
-    Node *l = nullptr, *r = nullptr;
-    ll sz = 0;
+#define    INF 1000000007
+#define  INF64 1000000000000000003LL
+#define    LMT 10000000
+#define  INPUT ios::sync_with_stdio(false); cin.tie(0);
 
-    Node(ll k){
-        key = k;
-        sz = 1;
-        prior = rand();
-    }
+#define    FST first
+#define    SND second
 
+#define    MOD 998244353LL
+
+#ifdef LOCAL
+    #define cout cerr
+#endif
+
+template<typename keytype>
+struct treap {
+	
+	treap *l = nullptr, *r = nullptr;
+	keytype key;
+	int prior, size;
+	
+	treap(keytype key0) {
+		key = key0;
+		prior = rand() * rand() + rand();
+		size = 1;
+	}
+	
 };
 
-ll size(Node *t){
-    if(t == nullptr){
-        return 0;
-    }
-    ll ls = 0, rs = 0;
-    if(t->l != nullptr){
-        ls = (t->l)->sz;
-    }
-    if(t->r != nullptr){
-        rs = (t->r)->sz;
-    }
-    return ls + rs + 1;
+template<typename keytype>
+treap<keytype>* merge(treap<keytype>* lt, treap<keytype>* rt) {
+	if (lt == nullptr) {
+		return rt;
+	}
+	if (rt == nullptr) {
+		return lt;
+	}
+	
+	if (lt->prior < rt->prior) {
+		rt->l = merge(lt, rt->l);
+		rt->size = size(rt);
+		return rt;
+	} else {
+		lt->r = merge(lt->r, rt);
+		lt->size = size(lt);
+		return lt;
+	}
 }
 
-Node* merge(Node *l, Node *r){
-    if(l == nullptr){
-        return r;
-    }
-    if(r == nullptr){
-        return l;
-    }
-    if(l->prior > r->prior){
-        l->r = merge(l->r, r);
-        l->sz = size(l);
-        return l;
-    }else{
-        r->l = merge(l, r->l);
-        r->sz = size(r);
-        return r;
-    }
+template<typename keytype>
+pair<treap<keytype>*, treap<keytype>*> split(treap<keytype>* t, keytype val) {
+	if (t == nullptr) {
+		return make_pair(nullptr, nullptr);
+	}
+	
+	if (t->key < val) {
+		auto [lt, rt] = split(t->r, val);
+		t->r = lt;
+		t->size = size(t);
+		return make_pair(t, rt);
+	} else {
+		auto [lt, rt] = split(t->l, val);
+		t->l = rt;
+		t->size = size(t);
+		return make_pair(lt, t);
+	}
 }
 
-pair<Node*, Node*> split(Node *t, ll val){
-    if(t == nullptr){
-        return make_pair(nullptr, nullptr);
-    }
-    if(val <= t->key){
-        auto [lt, rt] = split(t->l, val);
-        t->l = rt;
-        t->sz = size(t);
-        return make_pair(lt, t);
-    }else{
-        auto [lt, rt] = split(t->r, val);
-        t->r = lt;
-        t->sz = size(t);
-        return make_pair(t, rt);
-    }
+template<typename keytype>
+treap<keytype>* insert(treap<keytype>* t, keytype val) {
+	auto [lt, rt] = split(t, val);
+	treap<keytype> *node = new treap<keytype>(val);
+	return merge(merge(lt, node), rt);
 }
 
-Node* insert(Node *t, ll val){
-    auto [lt, rt] = split(t, val);
-    Node *node = new Node(val);
-    return merge(lt, merge(node, rt));
+template<typename keytype>
+treap<keytype>* erase(treap<keytype>* t, keytype val) {
+	auto [lt, mt] = split(t, val);
+	auto [_, rt] = split(mt, val + 1);
+	return merge(lt, rt);
 }
 
-ll min(Node *t){
-    if(t->l == nullptr){
-        return t->key;
-    }
-    return min(t->l);
+template<typename keytype>
+void elems(treap<keytype>* t) {
+	if (t == nullptr) {
+		return ;
+	}
+	elems(t->l);
+	cout << t->key << ' ';
+	elems(t->r);
 }
 
-ll count_less_equal(Node *t, ll val){
-    if(t == nullptr){
-        return 0;
-    }
-    if(t->key <= val){
-        //all keys in left <= val
-        return size(t->l) + count_less_equal(t->r, val) + 1;
-    }else{
-        //otherwise val < all keys in right
-        return count_less_equal(t->l, val);
-    }
+template<typename keytype>
+int size(treap<keytype>* t) {
+	int ls, rs;
+	if (t == nullptr) {
+		return 0;
+	}
+	if (t->l != nullptr) {
+		ls = t->l->size;
+	} else {
+		ls = 0;
+	}
+	if (t->r != nullptr) {
+		rs = t->r->size;
+	} else {
+		rs = 0;
+	}
+	return ls + rs + 1;
 }
 
-int main(){
-    input();
-    ll n, i, cur;
-    Node *t = nullptr;
-    cin >> n;
-    for(i = 0; i < n; i++){
-        cin >> cur;
-        t = insert(t, cur);
+template<typename keytype>
+int count_less(treap<keytype>* t, keytype val) {
+	if (t == nullptr) {
+		return 0;
+	}
+	if (t->key < val) {
+		return size(t->l) + count_less(t->r, val) + 1;
+	} else {
+		return count_less(t->l, val);
+	}
+}
+
+template<typename keytype>
+int count_greater(treap<keytype>* t, keytype val) {
+	if (t == nullptr) {
+		return 0;
+	}
+	if (t->key < val) {
+		return count_greater(t->r, val);
+	} else {
+		return count_greater(t->l, val) + size(t->r) + 1;
+	}
+}
+
+void solve () {
+    
+}
+
+int main() {
+ 
+    #ifndef LOCAL
+        INPUT;
+    #endif
+	
+    int t;
+ 
+    for (t = 1; t; t--) {
+        solve();
     }
-    cout << size(t) << "\n";
-    cout << min(t) << "\n";
-    cout << count_less_equal(t, 5) << "\n";
+ 
+    #ifdef LOCAL
+        cerr << '\n';
+        system("pause");
+    #endif
+ 
     return 0;
 }
