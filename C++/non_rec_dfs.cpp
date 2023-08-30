3f3e7a287c6ab46001ac7b17ed29a89d45e05b7e
Nikita <76213843+nikitaqqqq@users.noreply.github.com>
2023-08-30 17:03:20 +0500

    Update non_rec_dfs.cpp

--- C++/non_rec_dfs.cpp
+++ C++/non_rec_dfs.cpp
@@ -36,67 +36,67 @@ typedef long double ld;
 #endif
 
 void dfs(vector<vector<int>>& g, int s) {
-	
-	int v, u;
-	bool added;
-	vector<int> st;
-	vector<int> par;
-	
-	par.resize(g.size(), -1);
-	
-	st.push_back(s);
+    
+    int v, u;
+    bool added;
+    vector<int> st;
+    vector<int> par;
+    
+    par.resize(g.size(), -1);
+    
+    st.push_back(s);
     cout << s + 1 << '\n';
-	
-	while (!st.empty()) {
-		v = st.back();
-		
-		added = false;
-		while (!g[v].empty()) {
-			u = g[v].back();
-			
-			g[v].pop_back();
-			
-			if (par[v] == u) {
-				continue;
-			}
-			
-			par[u] = v;
-			added = true;
-			
-			st.push_back(u);
+    
+    while (!st.empty()) {
+        v = st.back();
+        
+        added = false;
+        while (!g[v].empty()) {
+            u = g[v].back();
+            
+            g[v].pop_back();
+            
+            if (par[v] == u) {
+                continue;
+            }
+            
+            par[u] = v;
+            added = true;
+            
+            st.push_back(u);
             cout << u + 1 << '\n';
             break;
-		}
-		
-		if (!added) {
-			st.pop_back();
+        }
+        
+        if (!added) {
+            st.pop_back();
             if (par[v] != -1) {
                 cout << par[v] + 1 << '\n';
             }
         }
         
-	}
-	
-	
+    }
+    
+    
 }
 
 void solve() {
-	
-	int n, m, i, v, u;
-	vector<vector<int>> g;
-	
-	cin >> n >> m;
-	
-	g.resize(n);
-	for (i = 0; i < m; i++) {
-		cin >> v >> u;
-		v--, u--;
-		
-		g[v].push_back(u);
-		g[u].push_back(v);
-	}
-	
-	dfs(g, 0);
+    
+    int n, m, i, v, u;
+    vector<vector<int>> g;
+    
+    cin >> n >> m;
+    
+    g.resize(n);
+    for (i = 0; i < m; i++) {
+        cin >> v >> u;
+        v--, u--;
+        
+        g[v].push_back(u);
+        g[u].push_back(v);
+    }
+    
+    dfs(g, 0);
     
 }
 
